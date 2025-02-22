a3c260bc683c8497a35602503807bd0b1e968040
Julian Rojas <jrojas@ditech.es>
2025-02-22 09:29:19 +0100

    Ajuste en llamada de función set por problemas en la prueba

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -2,8 +2,8 @@ import { useState } from "react";
 
 export const useCounter = (initialValue = 10) => {
     const [counter, setCounter] = useState(initialValue);
-    const increment = (i = 1) => setCounter(counter+i);
-    const decrement = (i = 1) => setCounter(counter-i);
+    const increment = (i = 1) => setCounter((current) => current+i);
+    const decrement = (i = 1) => setCounter((current) => current-i);
     const reset = () => setCounter(initialValue);
     return {
         counter,
